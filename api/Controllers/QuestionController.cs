b4f2d810f9f1b164428a15c36fed0baa334412dc
said.yeter <said.yeter@obss.tech>
2023-10-11 00:27:36 +0300

    Add an endpoint for getting questions on api #14

--- api/Controllers/QuestionController.cs
+++ api/Controllers/QuestionController.cs
@@ -27,4 +27,36 @@ public class QuestionController : ControllerBase
         await dbContext.SaveChangesAsync();
         return Ok(data);
     }
+
+    [HttpGet]
+    public async Task<IActionResult> GetQuestions([FromQuery] string ticket)
+    {
+        if (string.IsNullOrWhiteSpace(ticket))
+        {
+            return Unauthorized();
+        }
+        var participation = dbContext.Participations.Where(x => x.ParticipationTicket == ticket).FirstOrDefault();
+        if (participation is null)
+        {
+            return Unauthorized();
+        }
+
+        var survey = dbContext.Surveys.Where(x => x.Id == participation.SurveyId).FirstOrDefault();
+        if (survey is null || survey.EndDate < DateTime.Now)
+        {
+            dbContext.Participations.Remove(participation);
+            await dbContext.SaveChangesAsync();
+            return Unauthorized();
+        }
+
+        participation.StartDate = DateTime.Now;
+        await dbContext.SaveChangesAsync();
+
+        var questions = dbContext.Questions.Where(x => x.SurveyId == survey.Id);
+
+        return Ok(new
+        {
+            questions
+        });
+    }
 }
