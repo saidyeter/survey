3315601f3402d4c1b3cbd958d5867bda35deea57
said.yeter <said.yeter@obss.tech>
2023-10-11 00:29:39 +0300

    Enrich models
    Add a Email field to Participant on api #15
    Add a SurveyId and ParticipationTicket fields into Participation #16

--- api/DataAccess/Entities/Participant.cs
+++ api/DataAccess/Entities/Participant.cs
@@ -4,6 +4,7 @@ public class Participant
 {
     public int Id { get; set; }
 
+    public string Email { get; set; }
 
     public string Title { get; set; }
 
---
+++ api/Models/DTOs/SubmitAnswersReq.cs
@@ -0,0 +1,12 @@
+﻿namespace SurveyApi.Models.DTOs;
+
+public class SubmitAnswersReq
+{
+    #region Properties 
+    public SingleAnswer[] Answers { get; set; }
+
+    #endregion
+
+    #region Methods 
+    #endregion
+}
