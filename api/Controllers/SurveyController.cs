849a3df9ce7c580d5eb2d77655ee16f15cb1f7cc
said.yeter <said.yeter@obss.tech>
2023-10-18 02:40:13 +0300

    Add survey list on api #32 Add survey details on api #34 Add survey report on api #36

--- api/Controllers/SurveyController.cs
+++ api/Controllers/SurveyController.cs
@@ -3,6 +3,10 @@ using Microsoft.AspNetCore.OutputCaching;
 using Microsoft.EntityFrameworkCore;
 using SurveyApi.DataAccess;
 using SurveyApi.Models.DTOs;
+using static System.Net.Mime.MediaTypeNames;
+using System.Globalization;
+using System.Text;
+using SurveyApi.DataAccess.Entities;
 
 namespace SurveyApi.Controllers;
 
@@ -19,8 +23,8 @@ public class SurveyController : ControllerBase
         this.dbContext = dbContext;
     }
 
-    [OutputCache(Duration = 60 * 60)] // 1 saatligine cache yapar
-    [HttpGet]
+    //[OutputCache(Duration = 60 * 60)] // 1 saatligine cache yapar
+    [HttpGet("active-survey")]
     public async Task<IActionResult> GetActiveSurvey()
     {
         var now = DateTime.Now;
@@ -33,4 +37,257 @@ public class SurveyController : ControllerBase
         }
         return Ok(val);
     }
+
+    [HttpGet("all")]
+    public async Task<IActionResult> GetSurveys()
+    {
+        var val = await dbContext.Surveys
+            .OrderBy(s => s.StartDate)
+            .ToListAsync();
+        if (val is null)
+        {
+            return NotFound();
+        }
+        return Ok(new
+        {
+            surveys = val
+        });
+    }
+
+    [HttpGet("details")]
+    public async Task<IActionResult> GetSurveyDetails([FromQuery] int surveyId)
+    {
+        var survey = await dbContext.Surveys
+            .Where(s => s.Id == surveyId)
+            .FirstOrDefaultAsync();
+        if (survey is null)
+        {
+            return NotFound();
+        }
+
+
+        var allParticipantCount = await dbContext.Participants.CountAsync();
+        var partipicions = await dbContext.Participations
+            .Where(p => p.SurveyId == surveyId)
+            .ToListAsync();
+        var partipicionIdList = partipicions.Select(p => p.Id).ToList();
+
+        var questions = await dbContext.Questions
+            .Where(q => q.SurveyId == surveyId)
+            .ToListAsync();
+        var questionIdList = questions.Select(p => p.Id).ToList();
+        var answers = await dbContext.Answers
+            .Where(a => questionIdList.Contains(a.QuestionId))
+            .ToListAsync();
+
+        var participantAnswers = dbContext.ParticipantAnswers.Where(pa => partipicionIdList.Contains(pa.ParticipationId));
+        var report = from p in partipicions
+                     join pa in participantAnswers on p.Id equals pa.ParticipationId
+                     join q in questions on pa.QuestionId equals q.Id
+                     join a in answers on pa.QuestionId equals a.QuestionId
+                     group new { p, pa, q, a } by a.QuestionId into grouped
+                     select new QuestionDetail
+                     {
+                         Question = grouped.First().q,
+                         AnsweredCount = 0,
+                         AnswerDetails = grouped
+                            .GroupBy(gpd => gpd.a.Id)
+                            .Select(gpd =>
+                                new AnswerDetail
+                                {
+                                    Label = gpd.First().a.Label,
+                                    Text = gpd.First().a.Text,
+                                    ChoosenCount = 0
+                                })
+                            .ToArray()
+                     };
+
+        return Ok(new SurveyDetail
+        {
+            SurveyId = surveyId,
+            AllParticipantCount = allParticipantCount,
+            ParticipationCount = partipicions.Count,
+            QuestionDetails = report.ToArray()
+        });
+    }
+
+    [HttpGet("temp")]
+    public async Task<IActionResult> Seed()
+    {
+        var random = new Random();
+        var faker = new Faker();
+
+        var participants = new List<Participant>();
+
+        foreach (var u in faker.GetFakeUser())
+        {
+            var name = random.Next(1, 10) % 2 == 0 ? u.FirstName : u.LastName;
+            var title = random.Next(1, 10) % 2 == 0 ? "ECZANE " + name : name + " ECZANESI";
+            participants.Add(new Participant
+            {
+                Address = "address ",
+                Email = u.Email,
+                LegalIdentifier = u.Id,
+                Title = title,
+            });
+        }
+        dbContext.AddRange(participants);
+
+        var surveys = new List<Survey>();
+
+        var months = new[] { "", "OCAK", "ŞUBAT", "MART", "NİSAN", "MAYIS", "HAZİRAN", "TEMMUZ", "AĞUSTOS", "EYLÜL", "EKİM", "KASIM", "ARALIK", };
+        var now = DateTime.Now;
+        var surveyDate = new DateTime(now.Year, now.Month, 1);
+        for (int i = 0; i < 15; i++)
+        {
+            surveyDate = surveyDate.AddMonths(-2);
+            var startdate = surveyDate.AddMonths(-1);
+            surveys.Add(new Survey
+            {
+                StartDate = startdate,
+                EndDate = startdate.AddMonths(1).AddDays(-1),
+                Name = $"{startdate.Year} {months[startdate.Month]} ayi verimlilik anketi",
+                Description = $"Bu anketle, {startdate.Year} {months[startdate.Month]} ve oncesindeki birkac ayin verimlilik durumunu gozlemlemeyi planliyoruz",
+                Status = "ended"
+            });
+        }
+        dbContext.Surveys.AddRange(surveys);
+        await dbContext.SaveChangesAsync();
+
+
+        var answerLabels = "ABCDEFGHIJKLMNOPRSTUVYZ";
+
+        foreach (var s in surveys)
+        {
+            var qCounter = 1;
+            foreach (var fq in faker.GetFakeQuestions())
+            {
+                var q = new Question()
+                {
+                    OrderNumber = qCounter++,
+                    Text = fq.Text,
+                    SurveyId = s.Id,
+                    Required = fq.Required,
+                    AnswerType = AnswerType.Single,
+                };
+                dbContext.Add(q);
+
+                await dbContext.SaveChangesAsync();
+
+                var aCounter = 0;
+                foreach (var an in fq.Answers)
+                {
+                    dbContext.Add(new Answer
+                    {
+                        Label = answerLabels[aCounter++].ToString(),
+                        QuestionId = q.Id,
+                        Text = an
+                    });
+                }
+                await dbContext.SaveChangesAsync();
+            }
+        }
+
+        foreach (var s in surveys)
+        {
+            var pCount = participants.Count;
+            var length = pCount
+                - s.StartDate?.Month * 28
+                - (s.StartDate?.Month % 3 == 0 ? 500 : 0)
+                - (s.StartDate?.Month % 5 == 0 ? 300 : 0)
+                - (s.StartDate?.Month % 7 == 0 ? 100 : 0);
+
+            var picked = new HashSet<int>();
+            for (var i = 0; i < length; i++)
+            {
+                var pId = random.Next(pCount - 1);
+                while (picked.Any(p => p == pId))
+                {
+                    pId = random.Next(pCount - 1);
+                }
+                picked.Add(pId);
+                var d = pId % 2 == 0 ? 3 : 5;
+                dbContext.Add(new Participation
+                {
+                    StartDate = s.StartDate.Value.Date.AddDays(d),
+                    EndDate = s.EndDate.Value.Date.AddDays(-1 * d),
+                    PartipiciantId = pId,
+                    SurveyId = s.Id,
+                    ParticipationTicket = Guid.NewGuid().ToString().Replace("-", "").ToLower(),
+                    AdditionalWords = string.Empty
+                });
+            }
+        }
+
+        await dbContext.SaveChangesAsync();
+
+        var allQuestions = dbContext.Questions.ToList();
+        var allAnswers = dbContext.Answers.ToList();
+
+        foreach (var participation in dbContext.Participations.ToList())
+        {
+            var qs = allQuestions
+                .Where(q => q.SurveyId == participation.SurveyId)
+                .ToList();
+
+            var questionIdList = qs.Select(p => p.Id).ToList();
+            var answers = allAnswers
+                .Where(a => questionIdList.Contains(a.QuestionId))
+                .ToList();
+
+            var counter = 0;
+            foreach (var q in qs)
+            {
+                if (!q.Required && random.Next(1, 10) % 2 == 0)
+                {
+                    continue;
+                }
+                var currentAnswers = answers.Where(a => a.QuestionId == q.Id).ToList();
+
+                var answer = currentAnswers[random.Next(0, currentAnswers.Count)];
+
+                dbContext.ParticipantAnswers.Add(new ParticipantAnswer
+                {
+                    QuestionId = q.Id,
+                    AnswerId = answer.Id,
+                    ParticipationId = participation.Id,
+                });
+                counter++;
+            }
+
+            if (counter > 2000)
+            {
+                await dbContext.SaveChangesAsync();
+                counter = 0;
+            }
+        }
+
+        await dbContext.SaveChangesAsync();
+
+        return Ok();
+    }
+
+}
+
+public class QuestionDetail
+{
+    public Question Question { get; set; }
+    public AnswerDetail[] AnswerDetails { get; set; }
+    public int AnsweredCount { get; set; }
+
+
+}
+public class AnswerDetail
+{
+    public string Label { get; set; }
+    public string Text { get; set; }
+    public int ChoosenCount { get; set; }
+}
+
+public class SurveyDetail
+{
+    public int SurveyId { get; set; }
+    public int AllParticipantCount { get; set; }
+    public int ParticipationCount { get; set; }
+    public QuestionDetail[] QuestionDetails { get; set; }
 }
