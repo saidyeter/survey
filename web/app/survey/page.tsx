d617795d724079fc4595d55e4d52a85939062338
said.yeter <said.yeter@obss.tech>
2023-11-20 20:16:34 +0300

    Navigate to admin or survey on root #97

--- web/app/survey/page.tsx
+++ web/app/survey/page.tsx
@@ -1,26 +1,68 @@
-import ParticipantValidation from "@/components/participant-validation"
+import Link from "next/link"
+
+import { siteConfig } from "@/config/site"
+import { buttonVariants } from "@/components/ui/button"
 import { getRunningSurvey } from "@/lib/source-api"
 
 export default async function Survey() {
-    const survey = await getRunningSurvey()
+  const survey = await getRunningSurvey()
 
-    if (!survey) {
-        return (
-            <p className="max-w-[700px] text-lg text-muted-foreground">
-                Şu an aktif bir anket bulunmamaktadır.<br className="hidden sm:inline" />
-                Anket açıldığı zaman size bildirilecektir.
-            </p>
-        )
-    }
-    return (
-        <>
+  return (
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <div className="flex max-w-[980px] flex-col items-start gap-2">
+
+        {survey ? (
+          <>
             <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-                {survey.name}
+              {survey.name}
             </h1>
             <p className="max-w-[700px] text-lg text-muted-foreground">
-                {survey.description}
+              {survey.description}
             </p>
-            <ParticipantValidation />
-        </>
-    )
+          </>
+
+        ) : (
+          <>
+            <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+              Eczacı anketine hoşgeldiniz.
+            </h1>
+            <p className="max-w-[700px] text-lg text-muted-foreground">
+              Şu an aktif bir anket bulunmamaktadır.<br className="hidden sm:inline" />
+              Anket açıldığı zaman size bildirilecektir.
+            </p>
+
+          </>
+
+        )}
+      </div>
+      <div className="flex gap-4">
+        {survey ? (
+          <Link
+            href='/survey/start'
+            className={buttonVariants()}
+          >
+            Ankete Başla
+          </Link>
+        ) : (
+          <>
+
+            <Link
+              href={siteConfig.links.mail}
+              className={buttonVariants()}
+            >
+              E-Posta ile Ulaş
+            </Link>
+            <Link
+              href={siteConfig.links.phone}
+              className={buttonVariants({ variant: "outline" })}
+            >
+              Telefon ile Ulaş
+            </Link></>
+
+        )}
+
+
+      </div>
+    </section>
+  )
 }
