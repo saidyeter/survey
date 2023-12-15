bd1d25c9c362a9db87ad99506cb52ed309dd5e16
said.yeter <said.yeter@obss.tech>
2023-12-15 12:40:29 +0300

    Fix api routes

--- web/app/api/partipiciant/route.ts
+++ web/app/api/partipiciant/route.ts
@@ -1,21 +1,22 @@
 import { validateParticipant } from "@/lib/source-api"
 import { partipiciantValidationSchema } from "@/lib/types"
+import { NextResponse } from "next/server"
 
 export async function POST(request: Request) {
   const body = await request.json()
   const result = partipiciantValidationSchema.safeParse(body)
   if (!result.success) {
-    return Response.json({}, { status: 400 })
+    return  NextResponse.json({}, { status: 400 })
   }
   const res = await validateParticipant(result.data.email, result.data.code)
 
   if (!res) {
-    return Response.json({}, { status: 400 })
+    return  NextResponse.json({}, { status: 400 })
   }
   else if (res.participationTicket.length == 0) {
-    return Response.json({}, { status: 208 })
+    return  NextResponse.json({}, { status: 208 })
   }
 
   const cookie = `ticket=${res.participationTicket}; max-age:${60 * 60}; path=/; samesite=lax; httponly`
-  return Response.json({}, { status: 200, headers: { 'Set-Cookie': cookie }, })
+  return  NextResponse.json({}, { status: 200, headers: { 'Set-Cookie': cookie }, })
 }
