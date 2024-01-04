6a01fca4078e975a3643752611d165611640043b
said.yeter <said.yeter@obss.tech>
2024-01-04 21:39:32 +0300

    Fix mobile survey page

--- web/app/survey/layout.tsx
+++ web/app/survey/layout.tsx
@@ -4,17 +4,19 @@ interface RootLayoutProps {
 }
 export default async function SurveyLayout({ children }: RootLayoutProps) {
   return (
-    <div className="container-fluid full-height w-full pr-3 pl-3 mr-auto ml-auto">
-      <div className="row row-height h-[100vh] flex flex-wrap mt-0 mr-[-.75rem] ml-[-.75rem] ">
-        <div className="col-lg-6 content-left bg-[#010101] max-w-full flex-grow-0 flex-shrink-0 basis-auto w-1/2 content-left-wrapper flex justify-center items-center h-full max-h-[100vh] p-16 relative  bg-gradient-to-b from-transparent to-black/50 m-auto ">
+    <div className="w-full pr-3 pl-3 mr-auto ml-auto">
+      <div className="h-[100vh] flex flex-wrap mt-0 mr-[-.75rem] ml-[-.75rem] ">
+        <div className="hidden md:bg-[#010101] md:max-w-full md:flex-grow-0 md:flex-shrink-0 md:basis-auto md:w-1/2 md:content-left-wrapper md:flex md:justify-center md:items-center md:h-full md:max-h-[100vh] md:p-16 md:relative md:bg-gradient-to-b md:from-transparent md:to-black/50 md:m-auto ">
           <img src="/afis.webp"
             alt="poster"
-            className="img-fluid max-h-[80vh] w-auto"
+            className="max-h-[80vh] w-auto"
           />
         </div>
 
-        <div className="col-lg-6 content-right flex items-center justify-center  w-1/2 p-12">
-          {children}
+        <div className="flex items-center justify-center w-full p-4 md:w-1/2 md:p-12 bg-[url('/afis.webp')] bg-cover md:bg-none">
+          <div className="bg-muted rounded-lg w-2/3 p-4 md:p-0 md:h-full md:w-full md:flex md:items-center md:justify-center md:bg-inherit" >
+            {children}
+          </div>
         </div>
       </div>
     </div>
